417c566e242d1ab2c99e1265651092eecde1361b
Artyom Kotovskiy <artyomkotovskiy@gmail.com>
2026-01-04 23:41:44 -0500

    Add navigate home on successful login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { login } from "../api/auth";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate()
+
   const handleLogin = async () => {
     try {
       await login(email, password);
-      alert("Logged in");
+      navigate("/")
     } catch (e) {
       alert("Invalid credentials");
       console.log(e)
