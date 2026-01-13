5407e30c7dc8fcf3ad8b106f97b22c5245540298
Artyom Kotovskiy <artyomkotovskiy@gmail.com>
2026-01-13 02:20:11 -0500

    Job click animation

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -11,8 +11,10 @@ export default function JobListItem({ job, selected, onClick }: Props) {
   return (
     <div
       onClick={onClick}
-      className={`p-4 border-b cursor-pointer hover:bg-gray-50 ${
-        selected ? "bg-blue-50 border-l-4 border-l-blue-500" : ""
+      className={`p-4 border-b cursor-pointer hover:bg-gray-50 
+                transition-all duration-150 ease-out active:scale-[0.99]
+                ${selected ? "bg-blue-50 border-l-4 border-l-blue-500" : ""
+        
       }`}
     >
       <h3 className="font-semibold">{job.title}</h3>
